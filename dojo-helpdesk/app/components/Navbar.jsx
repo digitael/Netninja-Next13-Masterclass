1bea580cf26488a7c2d2161a993b00dfe6ae0c5e
Christian van lierop <you@example.com>
2024-05-29 16:01:51 +0200

    Halfway lesson 11

--- dojo-helpdesk/app/components/Navbar.jsx
+++ dojo-helpdesk/app/components/Navbar.jsx
@@ -5,10 +5,16 @@ import Logo from './dojo-logo.png';
 export default function Navbar() {
   return (
     <nav>
-      <Image src={Logo} alt='Dojo-helpdesk logo' width={70} quality={100} placeholder='blur'></Image>
+      <Image
+        src={Logo}
+        alt='Dojo-helpdesk logo'
+        width={70}
+        quality={100}
+        placeholder='blur'
+      ></Image>
       <h1>Dojo Helpdesk</h1>
       <Link href='/'>Dashboard</Link>
-      <Link href='./tickets'>Tickets</Link>
+      <Link href='/tickets'>Tickets</Link>
     </nav>
   );
 }
--- dojo-helpdesk/app/tickets/TicketList.jsx
+++ dojo-helpdesk/app/tickets/TicketList.jsx
@@ -1,4 +1,8 @@
+import Link from 'next/link';
+
 async function getTickets() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
   const res = await fetch('http://localhost:4000/tickets', {
     next: {
       revalidate: 0, //opt out of caching data!
@@ -15,12 +19,18 @@ export default async function TicketList() {
     <>
       {tickets.map((ticket) => (
         <div key={ticket.id} className='card my-5'>
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
-      {tickets.length === 0 && <p className='text-center'>There are no open tickets, jay!</p>}
+      {tickets.length === 0 && (
+        <p className='text-center'>There are no open tickets, jay!</p>
+      )}
     </>
   );
 }
